195acc8e5416f408c3df9d8265bce3c6ad71e780
waizbart <guilhermewaizbart@gmail.com>
2026-03-28 20:43:23 -0300

    feat: update Open Graph image dimensions and add new gallery images

--- src/config/invite.config.ts
+++ src/config/invite.config.ts
@@ -67,6 +67,16 @@ export const inviteConfig = {
       '/images/gallery-3.jpg',
       '/images/gallery-4.jpg',
       '/images/gallery-5.jpg',
+      '/images/gallery-6.jpg',
+      '/images/gallery-7.jpg',
+      '/images/gallery-8.jpg',
+      '/images/gallery-9.jpg',
+      '/images/gallery-10.jpg',
+      '/images/gallery-11.jpg',
+      '/images/gallery-12.jpg',
+      '/images/gallery-13.jpg',
+      '/images/gallery-14.jpg',
+      '/images/gallery-15.jpg'
     ],
   },
 
